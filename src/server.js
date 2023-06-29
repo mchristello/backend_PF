15bc30d3b2a53877b4f8c265ae09787030c4c342
MatCh <m.christello@hotmail.com>
2023-06-28 21:43:58 -0300

    congif CORS_2

--- src/server.js
+++ src/server.js
@@ -32,10 +32,7 @@ const app = express();
 const httpServer = app.listen(config.PORT, console.log(`Server up & running on port ${config.PORT}`));
 
 
-app.use(cors({
-    "origin": true,
-    "methods": "GET,HEAD,PUT,POST,DELETE"
-}))
+app.use(cors())
 app.use(addLogger)
 app.use(errorHandler)
 app.use('/apiDocs', swaggerUiExpress.serve, swaggerUiExpress.setup(initSwagger()))
